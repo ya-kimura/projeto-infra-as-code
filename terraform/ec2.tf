4844d84d86fcdc0b6bb21fe4b26dc68dba49a7f3
yak.entelgy <yassui.fortes@entelgy.com>
2021-07-09 21:11:18 -0300

    feat: cria config dos modulos
    - ec2
    - security group
    - providers
    - data
    - variable
    - output

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,18 +1,6 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  
-  filter {
-    name   = "name"
-    values = ["nodezin-${var.hash_commit}"] 
-  }
-
-  owners = ["self"] # my user
-}
-
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  subnet_id     = "subnet-"
   vpc_security_group_ids = ["${aws_security_group.allow_http.id}"]
  
   tags = {
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,7 @@
 output "ip_adress" {
   value = aws_instance.web[*].public_ip  
 }
+
+output "dns_adress" {
+  value = aws_instance.web[*].public_dns 
+}
