54f4afbea92dcf1f34d1b7a3c1ea320e0b01dbf4
yassui.kimura <yassui.fortes@outlook.com>
2021-10-21 17:36:16 -0300

    refactor: atualiza workflow e tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,15 +1,3 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  
-  filter {
-    name   = "name"
-    values = "nodezin_*"
-  }
-
-  owners = ["self"] # my user
- #owners = ["872388664293"] my user ami
-}
-
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
