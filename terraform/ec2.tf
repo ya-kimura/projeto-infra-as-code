c88e550b0c249a7caffe54560918da61464f3b2d
yassui.kimura <yassui.fortes@outlook.com>
2022-03-29 10:03:57 -0300

    chore: teste tflint

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
+  description            = "ami datasource"
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.allow_http.id}"]
 
